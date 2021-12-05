768adcbd81a2cd4f71e3333b0ccc652eb0c9ef9d
Anastasiia Rusanova <90505680+rusanovaA@users.noreply.github.com>
2021-12-05 22:36:09 +0300

    Add files via upload

---
+++ functional.py
@@ -0,0 +1,46 @@
+def sequential_map(*args):
+    num = list(args[-1])
+    for arg in args[:-1]:
+        res = list((map(arg, num)))
+        num = res
+    return list(map(int, res))
+
+
+def consensus_filter(*args):
+    num = list(args[-1])
+    for arg in args[:-1]:
+        res = list((filter(arg, num)))
+        num = res
+    return list(map(int, res))
+
+
+def conditional_reduce(fun_1, fun_2, num):
+    res = []
+    for i in range(len(num)):
+        if fun_1(num[i]) == True and fun_1(num[i + 1]):
+            res.append(fun_2(num[i], num[i + 1]))
+        else:
+            continue
+    return res
+
+
+def conditional_reduce(fun_1, fun_2, num):
+    res = []
+    fun_res = []
+    for i in range(len(num)):
+        if fun_1(num[i]) == True and fun_1(num[i + 1]) == True:
+            res.append(fun_2(num[i], num[i + 1]))
+        else:
+            continue
+    return sum(res)
+
+
+### I could only come up with non-working functions :(
+
+
+def func_chain(*args):
+    return [arg for arg in args]
+
+
+def func_chain(*args):
+    return args
