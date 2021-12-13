3ad665732a968db524e822702d41b0230158da01
Anastasiia Rusanova <90505680+rusanovaA@users.noreply.github.com>
2021-12-13 22:42:02 +0300

    Add files via upload

---
+++ ln.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+
+
+import os
+import sys
+import argparse
+
+
+parser = argparse.ArgumentParser(description="Create hard and symbolic link")
+parser.add_argument("target", type=str, help="Target")
+parser.add_argument("link", type=str, help="Link")
+parser.add_argument("-s", help="Create a symbolic link")
+args = parser.parse_args()
+
+if args.s:
+    os.symlink(args.target, args.link)
+esle:
+    os.link(args.target, args.link)
